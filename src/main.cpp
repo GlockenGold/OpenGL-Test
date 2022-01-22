61b150c574fa732e61189af3fe2c527488b4288e
Erik Halvarsson <glockengold19@.live.se>
2022-01-23 00:42:45 +0100

    Changed ordering in render loop.

--- src/main.cpp
+++ src/main.cpp
@@ -170,17 +170,20 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Activate the shader program.
+        glUseProgram(shaderProgram);
+
+        // Update the uniform colour.
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue)/2.0f) + 0.5f;
-        int vertexColorLocation =glGetUniformLocation(shaderProgram, "ourColor");
-    
-        // Draw our first triangle
-        glUseProgram(shaderProgram);
+        int vertexColorLocation =glGetUniformLocation(shaderProgram, "ourColor"); 
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // Draw the first triangle.
         glBindVertexArray(VAOs[0]);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw square using indices.
         glDrawArrays(GL_TRIANGLES, 4, 3);  // Since we have four vertices saved for the square, start at index 4.
-
+        // Draw the second triangle.
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES,0 ,3);
 
